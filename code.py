c614f05e83a54822fd47e74a7b117d13eddb6547
Faris Allafi <96661970+CoolCreator247@users.noreply.github.com>
2022-06-18 14:36:40 -0700

    Update code.py

--- code.py
+++ code.py
@@ -1,12 +1,9 @@
 def lexer():
      
-    tweet_input = "func:"
+    tweet_input = "func"
     
     # functions
-    result = tweet_input.find("func:")
-    print("test")
-lexer()    
+    result = tweet_input.find("func")
+    func = True
 
-    
-  
-    
+    result = tweet_input.find("")
